3f4478f97d7f2dfc54af897ab7be8b3c011bb820
K4KarolE <karoly.egyed@protonmail.com>
2022-11-05 09:49:34 +0000

    1st step

--- 26_chpt9_Regex_Search.py
+++ 26_chpt9_Regex_Search.py
@@ -1,4 +1,7 @@
+'''
 26 - Regex Search - Chapter 6 - Practice
 - ask the user the route to the folder
 - ask the user the regular expression the program should looking for
 - print the result(file name and matches) to the screen
+'''
+
