d136526af852b8671a5c0e00718cf3670ebeb05f
K4KarolE <karoly.egyed@protonmail.com>
2022-11-09 07:32:41 +0000

    clipboard

--- 27_TVMovie-TVSpecial_Details_Scraping.py
+++ 27_TVMovie-TVSpecial_Details_Scraping.py
@@ -15,7 +15,7 @@ from selenium import webdriver
 
 from datetime import date
 
-from tkinter import Tk
+import pyperclip as pc
 
 import sys, webbrowser
 
@@ -33,14 +33,14 @@ print()
 print(' Z-z-z '*k)
 print('\n')
 
-# link = input(' Please add the new Movie`s IMDb link: ')
-link = Tk().clipboard_get()     #taking the link from clipboard
+link = pc.paste()               #taking the link from clipboard
 cellnumber = 3
 
 PATH = 'C:\Program Files (x86)\chromedriver.exe'
 driver = webdriver.Chrome(PATH)
 driver.minimize_window() # looks way faster than headless chrome
 driver.get(link)
+
 
 # VARIABLES FOR THE READOUT - avoiding errors at the excel writing stage
 # - if the there are less than 3 records/types to add
@@ -242,7 +242,16 @@ ws[day].value = formula
 cellRFirst = 'O' + str(cellnumber)
 ws[cellRFirst].value = '1st'        
 
-wb.save('D:/Movies_New_Record.xlsx')
+# SAVE THE SHEET
+openSheet = True
+while openSheet == True:
+        try:
+                wb.save('D:/Movies_New_Record.xlsx')
+                openSheet = False
+                print('\n')
+        except:
+                print()
+                input('!!! ERROR - Close your sheet and hit Enter !!!')
 
 # POSTER IMAGE
 try:
