14ba84d95efd05432baea60d431e9dcb8eb6c40c
K4KarolE <karoly.egyed@protonmail.com>
2022-11-10 09:32:43 +0000

    prep

--- 26_chpt9_Regex_Search.py
+++ 26_chpt9_Regex_Search.py
@@ -11,3 +11,16 @@ regex in file
 regex in folder
 
 '''
+
+import re
+
+list = ['bubuka@gmail.com','bubu.pa@gmail.com','bubu-ta@gmail.com','4',
+         '077 566 5654','077_566_5654', '0775665654','077-566-5654','random words', '554654654']
+
+phoneNumRegex = re.compile(r'\d\d\d.\d\d\d.\d\d\d') #
+for i in list:
+    searchPN = phoneNumRegex.search(i)
+    if phoneNumRegex.search(i):
+        print(searchPN.group())
+    else:
+        print('No match')
