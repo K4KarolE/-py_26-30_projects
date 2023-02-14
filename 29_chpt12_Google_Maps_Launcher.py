5aa49957f3b9811ede6825c6ce3a48df712a756e
karoly.egyed@protonmail.com <karoly.egyed@protonmail.com>
2023-02-14 13:19:52 +0000

    direction update for job search

--- 29_chpt12_Google_Maps_Launcher.py
+++ 29_chpt12_Google_Maps_Launcher.py
@@ -2,8 +2,8 @@
 29 Google Maps Launcher
 - user: copy the address we want to search for
 - user: start the script
-- take the address from clipboard
-- launch the browser / Google Maps with the address
+- it takes the address from clipboard
+- it launches Google Maps in a new browser tab displaying the direction between "your postcode" and the "target location"
 '''
 
 import pyperclip as pc
@@ -11,8 +11,10 @@ import webbrowser
 
 searchingFor = pc.paste()
 
-sepSFor = searchingFor.split()
+sepS_For = searchingFor.split()
 
-link = 'https://www.google.co.uk/maps/search/'+ '+'.join(sepSFor)
+your_postcode = "HP3+5BZ"       # example postcode
+
+link = f'https://www.google.co.uk/maps/dir/{your_postcode}/' + '+'.join(sepS_For)
 
 webbrowser.open(link)
